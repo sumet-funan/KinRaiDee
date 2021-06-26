67de66c6ddb5e14279c22e8fc4a35cc5cdff2bae
sfunan <zzc0412102@celestica.com>
2021-06-27 02:39:20 +0700

    First commit

---
+++ KinRaiDee.Web/Models/Cooking.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KinRaiDee.Web.Models
+{
+    public class Cooking
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("raw_material")]
+        public RawMaterial[] RawMaterial { get; set; }
+
+        [JsonPropertyName("cooking_method")]
+        public CookingMethod[] CookingMethod { get; set; }
+    }
+}
---
+++ KinRaiDee.Web/Models/RawMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KinRaiDee.Web.Models
+{
+    public class RawMaterial
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("volumn")]
+        public long Volumn { get; set; }
+
+        [JsonPropertyName("unit")]
+        public string Unit { get; set; }
+    }
+}
